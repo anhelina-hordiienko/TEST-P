c26df137c335190f080ffb70f05eaa502410f891
anhelina-hordiienko <anhelina.hordiienko@gmail.com>
2019-12-23 13:30:00 +0200

    add dynamic rating

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const slides = [
         },
         title: 'Full Spectrum CBD Oil',
         volume: '30 ML',
-        price: '$28.95'
+        price: '$28.95',
+        rating: 4
     },
     {
         img: {
@@ -15,7 +16,8 @@ const slides = [
         },
         title: 'Full Spectrum CBD Oil 50ml',
         volume: '50 ML',
-        price: '$38.95'
+        price: '$38.95',
+        rating: 5
     },
     {
         img: {
@@ -24,7 +26,8 @@ const slides = [
         },
         title: 'Full Spectrum CBD Oil 100ml',
         volume: '100 ML',
-        price: '$58.95'
+        price: '$58.95',
+        rating: 3
     }
 ]
 
@@ -34,17 +37,27 @@ let image = document.getElementById('arrivals-image');
 let title = document.getElementById('title');
 let volume = document.getElementById('volume');
 let price = document.getElementById('price');
+let starsArray = document.querySelectorAll('#rating-stars img');
 
-const slideInfo = function() {
-    slide.innerText = slideNumber+1;
+const slideInfo = function () {
+    slide.innerText = slideNumber + 1;
     image.src = slides[slideNumber].img.src;
     image.alt = slides[slideNumber].img.alt;
     title.innerText = slides[slideNumber].title;
     volume.innerText = slides[slideNumber].volume;
     price.innerText = slides[slideNumber].price;
+    starsArray.forEach((starImg, starImgIndex) => {
+        if (starImgIndex < slides[slideNumber].rating) {
+            starImg.src = './img/green star.png';
+            starImg.alt = 'green star';
+        } else {
+            starImg.src = './img/gray star.png';
+            starImg.alt = 'gray star';
+        }
+    });
 }
 
-const nextSlideInfo = function() {
+const nextSlideInfo = function () {
     slideNumber++;
     if (slideNumber == slides.length) {
         slideNumber = 0;
@@ -52,10 +65,10 @@ const nextSlideInfo = function() {
     slideInfo();
 }
 
-const prevSlideInfo = function() {
+const prevSlideInfo = function () {
     slideNumber--;
     if (slideNumber < 0) {
-        slideNumber = slides.length-1;
+        slideNumber = slides.length - 1;
     }
     slideInfo();
 }
